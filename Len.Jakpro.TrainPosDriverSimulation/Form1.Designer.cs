9325b62f1e4efa1c0ac13bd5e3f88dabd247e52f
Dobby Akhmadi PC <dobby.akhmadi@gmail.com>
2020-03-25 12:14:57 +0700

    News Update
    update db from OCC and add new simulator for OA driver

--- Len.Jakpro.TrainPosDriverSimulation/Form1.Designer.cs
+++ Len.Jakpro.TrainPosDriverSimulation/Form1.Designer.cs
@@ -29,93 +29,153 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Host = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.BtnStart = new System.Windows.Forms.Button();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Interval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.Port);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.Host);
+            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.BtnStart);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.Interval);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(456, 12);
+            this.panel1.Location = new System.Drawing.Point(518, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(278, 255);
             this.panel1.TabIndex = 1;
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Set Interval";
+            this.button1.Location = new System.Drawing.Point(135, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Stop Simulation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // Port
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(92, 20);
-            this.textBox1.TabIndex = 4;
+            this.Port.Location = new System.Drawing.Point(93, 69);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(92, 20);
+            this.Port.TabIndex = 14;
+            this.Port.Text = "1504";
             // 
-            // label2
+            // label9
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(223, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Ms";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Port RX OA";
             // 
-            // panel2
+            // Host
             // 
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(456, 414);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(278, 166);
-            this.panel2.TabIndex = 6;
+            this.Host.Location = new System.Drawing.Point(93, 45);
+            this.Host.Name = "Host";
+            this.Host.Size = new System.Drawing.Size(160, 20);
+            this.Host.TabIndex = 8;
+            this.Host.Text = "127.0.0.1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Host/IP";
             // 
             // BtnStart
             // 
-            this.BtnStart.Location = new System.Drawing.Point(10, 186);
+            this.BtnStart.Location = new System.Drawing.Point(29, 143);
             this.BtnStart.Name = "BtnStart";
-            this.BtnStart.Size = new System.Drawing.Size(109, 61);
+            this.BtnStart.Size = new System.Drawing.Size(100, 35);
             this.BtnStart.TabIndex = 6;
             this.BtnStart.Text = "Start Simulation";
             this.BtnStart.UseVisualStyleBackColor = true;
             this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ms";
+            // 
+            // Interval
+            // 
+            this.Interval.Location = new System.Drawing.Point(93, 93);
+            this.Interval.Name = "Interval";
+            this.Interval.Size = new System.Drawing.Size(92, 20);
+            this.Interval.TabIndex = 4;
+            this.Interval.Text = "2000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Set Interval";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(132, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Not Connected";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Connection Status  :";
+            // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.dataGridView1);
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Controls.Add(this.listView1);
             this.panel3.Location = new System.Drawing.Point(12, 12);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(438, 568);
+            this.panel3.Size = new System.Drawing.Size(489, 568);
             this.panel3.TabIndex = 7;
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(3, 23);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(428, 542);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // label3
             // 
@@ -126,31 +186,50 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Log Simulation";
             // 
-            // label4
+            // label7
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(19, 22);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Connection Status  :";
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Configuration";
             // 
-            // label5
+            // Column2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(122, 22);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(79, 13);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Not Connected";
+            this.Column2.HeaderText = "Description";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 280;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Date";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 150;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(473, 545);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 592);
+            this.ClientSize = new System.Drawing.Size(814, 592);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -160,10 +239,9 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -171,15 +249,22 @@
         #endregion
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Interval;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.TextBox Host;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
 
--- Len.Jakpro.TrainPosDriverSimulation/Form1.cs
+++ Len.Jakpro.TrainPosDriverSimulation/Form1.cs
@@ -1,42 +1,180 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Timers;
-using Timer = System.Timers.Timer;
+using System.Data;
+using SimpleTCP;
+using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
 
 namespace Len.Jakpro.TrainPosDriverSimulation
 {
     public partial class Form1 : Form
     {
-    
+        private System.Timers.Timer _Timers;
+        private int Ports;
+        private string IP;
+        
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();                 
         }
-        private static Timer timer;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer = new System.Timers.Timer();
-            timer.Interval = 5000;
+            button1.Enabled = false;
 
-            timer.Elapsed += OnTimedEvent;
-       //     timer.AutoReset = true;
-            timer.Enabled = true;
+            label5.ForeColor = Color.OrangeRed;
+            label5.Text = "Not Running";
+        }
 
-            listView1.View = View.Details;
-            listView1.Columns.Add("Date");
-            listView1.Columns.Add("Description");
-            listView1.GridLines = true;         
-        } 
+        private void ValidationEnable(bool x)
+        {
+            Interval.Enabled = x;
+            Host.Enabled = x;
+            Port.Enabled = x;
+            button1.Enabled =  ! x;
+            BtnStart.Enabled = x;
+        }
 
         private void BtnStart_Click(object sender, EventArgs e)
         {
-        
+            Ports = Convert.ToUInt16(Port.Text);
+            IP = Host.Text;
+
+
+            label5.ForeColor = Color.Green;
+            label5.Text = "Running";
+
+            _Timers = new System.Timers.Timer(Convert.ToDouble(Interval.Text));
+            _Timers.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            _Timers.Enabled = true;
+            _Timers.Start();
+
+            ValidationEnable(false);
         }
-        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {                    
+            try
+            {
+                if (dataGridView1.InvokeRequired)
+                {
+                    dataGridView1.Invoke(new Action(SendData));
+                    return;
+                }                
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Rows.Add(DateTime.Now.ToString(), ex.ToString());
+            }
+        }
+        public string TranslateNumberTrain(int train)
         {
-            listView1.Items.Add(new ListViewItem(new string[] { "tes", "11234" }));
+            if (train == 2)
+            {
+                return "Pegangsaan Dua";
+            }
+            else if (train == 3)
+            {
+                return "Boulevard Utara";
+            }
+            else if (train == 4)
+            {
+                return "Boulevard Selatan";
+            }
+            else if (train == 5)
+            {
+                return "Pulomas";
+            }
+            else if (train == 6)
+            {
+                return "Equestrian";
+            }
+            else if (train == 7)
+            {
+                return "Velodrome";
+            }
+            return "";
         }
-  
+        public string TranslatePlatformTrain(int Platform)
+        {
+            if (Platform == 1)
+            {
+                return "East";
+            }
+            else if (Platform == 2)
+            {
+                return "West";
+            }            
+            return "";
+        }
+        public string TranslateTypeMessageTrain(int Message)
+        {
+            if (Message == 1)
+            {
+                return "Approach";
+            }
+            else if (Message == 2)
+            {
+                return "Arrival";
+            }
+            else if (Message == 3)
+            {
+                return "Depart";
+            }
+            return "";
+        }
+        private void SendData()
+        {
+            if (dataGridView1.Rows.Count <= 25)
+            {
+                SimpleTcpClient _sendvalues;
+                try
+                {
+                    Random rnd1 = new Random();
+                    int Location = rnd1.Next(2, 7);
+                    Random rnd2 = new Random();
+                    int Platform = rnd2.Next(1, 2);
+                    Random rnd3 = new Random();
+                    int TypeMessage = rnd3.Next(1, 3);
+                    Random rnd4 = new Random();
+                    int TrainNumb = rnd4.Next(1001,1010);
+                    Random rnd5 = new Random();
+                    int TrainID = rnd5.Next(100, 999);
+
+                    string date = DateTime.Now.ToString("dd:MM:yyyy");
+
+                    _sendvalues = new SimpleTcpClient().Connect(IP,Ports);
+                    _sendvalues.Write("PASignalling,"+Location+","+ Platform +","+ TypeMessage+"," + date + "," + TrainNumb + "," + TrainID + "");
+
+                    dataGridView1.Rows.Add(DateTime.Now.ToString("dd:MM:yyyy hh:mm:ss"), TranslateNumberTrain(Location)+"-"+ TranslatePlatformTrain(Platform) + "-" + TranslateTypeMessageTrain(TypeMessage) + "-" + TrainNumb + "-" + TrainID);
+
+                    _sendvalues.Dispose();
+                    _sendvalues.Disconnect();
+                }
+                catch (SocketException e)
+                {
+                    dataGridView1.Rows.Add(DateTime.Now.ToString(), string.Concat(e.StackTrace, e.Message));
+                }         
+            }
+            else
+            {
+                dataGridView1.Rows.Clear();
+            }           
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _Timers.Stop();
+            ValidationEnable(true);
+
+            label5.ForeColor = Color.OrangeRed;
+            label5.Text = "Not Running";
+        }
     }
 }
